f7163d8e1404be1f9df54e9453cb30ea9c015819
defihumair <97887887+defihumair@users.noreply.github.com>
2025-07-05 18:28:36 +0500

    vessel.py

--- vessel.py
+++ vessel.py
@@ -9,7 +9,8 @@ from io import BytesIO
 
 # Load data
 file_path = 'Ametist.xlsx'
-df = pd.read_excel(file_path)
+sheet_name = 'Sheet1'  # Adjust if needed
+data = pd.read_excel(file_path, sheet_name=sheet_name)
 df = df[['Vessel', 'Port of loading', 'Unloading port', 'Departure', 'Arrival']]
 df.dropna(inplace=True)
 
